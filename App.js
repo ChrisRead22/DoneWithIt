1268c8ecdedde31ea5c14e5bab4eed6a3ecc7a28
Christopher Read <Christopherread22@outlook.com>
2021-12-08 21:33:20 -0500

    Starting

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 
 // View -> UIView
 export default function App() {
+  console.log("App executed");
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
