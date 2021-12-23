2187bef57079fd21ec43e86e7e14584a304bc902
Christopher Read <Christopherread22@outlook.com>
2021-12-22 19:07:40 -0500

    cleaning up

--- App.js
+++ App.js
@@ -11,15 +11,7 @@ export default function App() {
         Hello React Native
         </Text>
         <TouchableNativeFeedback onPress={() => console.log("Image tapped")}>
-        <Image 
-        blurRadius={10}
-        fadeDuration={1000}
-        source={{
-          width: 200,
-          height: 300,
-          uri: "https://picsum.photos/200/300",
-          }}
-         />
+        
         </TouchableNativeFeedback>
       <StatusBar style="auto" />
     </SafeAreaView>
