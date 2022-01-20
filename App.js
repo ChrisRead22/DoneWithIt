12eb9e7a535f56dde16ec0e06c1cd3dcb3067a33
Christopher Read <Christopherread22@outlook.com>
2022-01-19 20:22:47 -0500

    justifyContent

--- App.js
+++ App.js
@@ -19,6 +19,7 @@ export default function App() {
           backgroundColor: '#fff',
           flex: 1,
           flexDirection: "row",
+          justifyContent: "center", // main axis
         }}
         >
           <View style={{
