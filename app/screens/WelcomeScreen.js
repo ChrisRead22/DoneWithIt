2fdde8f81bd04237010b94a6be2f7968b1eb1482
Christopher Read <Christopherread22@outlook.com>
2022-01-28 18:06:30 -0500

    welcome

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -3,13 +3,17 @@ import { ImageBackground, StyleSheet } from 'react-native';
 
 function WelcomeScreen(props) {
     return (
-       <ImageBackground source={require('../assets/background.jpg')}
+       <ImageBackground
+       style={styles.background}
+        source={require('../assets/background.jpg')}
        ></ImageBackground>
     );
 }
 
 const styles = StyleSheet.create({
-
+    background: {
+        flex: 1
+    }
 })
 
 export default WelcomeScreen;
