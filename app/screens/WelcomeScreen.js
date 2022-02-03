e4cc1d4675279225aa3aed829a4ddfa41be5bc76
Christopher Read <Christopherread22@outlook.com>
2022-02-02 20:38:51 -0500

    button

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -23,6 +23,11 @@ const styles = StyleSheet.create({
         height: 70,
         backgroundColor: '#fc5c65',
     },
+    registerButton: {
+        width: "100%",
+        height: 70,
+        backgroundColor: "#4ecdc4",
+    },
 });
 
 export default WelcomeScreen;
